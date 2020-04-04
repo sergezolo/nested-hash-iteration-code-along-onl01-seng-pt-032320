3f204920e8644e2e1d96dea41a6b27dea23b458f
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-04 00:52:11 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
+  if contacts["Freddy Mercury"][:favorite_ice_cream_flavors] == "strawberry"
+    
 
 end
